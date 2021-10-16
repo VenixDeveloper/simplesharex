c745a973432c3b13775865d56d729c2ce53bb1ea
Andreas C <me@venixdev.xyz>
2021-10-16 20:23:40 +0200

    Fix Discord Border

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const fs = require('fs-extra');
+const path = require('path');
 const fileUpload = require('express-fileupload');
 const app = express()
 
 const formatREGEX = /\.(gif|jpg|jpeg|tiff|png|mp4)$/i;
 
 app.use(fileUpload());
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('files', path.join(__dirname, 'files'));
 app.use('/api/', require('./routes/api'));
 
 app.get('/', (req, res) => {
@@ -19,12 +23,32 @@ app.get('/:file', async (req, res) => {
 
     const [, format] = formatREGEX.exec(req.params.file);
 
+    /*
+    if (format === 'gif') res.writeHead(200, { "Content-Type": "image/gif" });
+    else if (format === 'mp4') res.writeHead(200, { "Content-Type": "video/mp4" });
+    else res.writeHead(200, { "Content-Type": "image/png" });
+    */
+
+    const fileName = `${req.params.file}`;
+    const imgData = fs.readFileSync(`${process.cwd()}/src/files/${req.params.file}`);
+    res.render('file', {
+        fileName: fileName,
+        format: format
+    });
+    // return fs.createReadStream(`${process.cwd()}/src/files/${req.params.file}`).pipe(res);
+});
+
+app.get('/raw/:file', async (req, res) => {
+    const exist = await fs.exists(`${process.cwd()}/src/files/${req.params.file}`);
+    if (!exist) return res.status(404).json({ error: true, message: 'No file with such name was found!'});
+    const [, format] = formatREGEX.exec(req.params.file);
+
     if (format === 'gif') res.writeHead(200, { "Content-Type": "image/gif" });
     else if (format === 'mp4') res.writeHead(200, { "Content-Type": "video/mp4" });
     else res.writeHead(200, { "Content-Type": "image/png" });
 
     return fs.createReadStream(`${process.cwd()}/src/files/${req.params.file}`).pipe(res);
-});
+})
 
 app.get('*', (req, res) => {
     res.status(403).json({error: true, message: 'Unauthorized'})
